b0703ac75f14d6e1e10007fd45da346938399ac6
Sainath <10643404+reddysainathn@users.noreply.github.com>
2018-03-04 21:28:23 -0600

    ngFor directive Usage with Change Detection

--- src/app/components/ng-for-usage/ng-for-usage.component.ts
+++ src/app/components/ng-for-usage/ng-for-usage.component.ts
@@ -12,7 +12,16 @@ export class NgForUsageComponent implements OnInit {
     { id: 2, name: 'course2' },
     { id: 3, name: 'course3' }
   ];
-
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4' })
+  }
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1)
+  }
+  onChange(course) {
+    course.name = "Updated!";
+  }
   constructor() { }
 
   ngOnInit() {
